f76c289972d8fd0cf0b03e7c23bca692d55578e2
Tapiiaa <ptapilob@myuax.com>
2024-11-08 16:31:47 +0100

    xml, orientado a app medica. Login siempre inicia

--- app/src/main/java/com/example/appmedica/auth/LoginActivity.java
+++ app/src/main/java/com/example/appmedica/auth/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.appmedica.auth;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -16,18 +15,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-        SharedPreferences preferences = getSharedPreferences("auth_prefs", MODE_PRIVATE);
-        String savedUsername = preferences.getString("username", null);
-
-        if (savedUsername != null) {
-            // Usuario ya ha iniciado sesión anteriormente, redirigir a MainActivity
-            navigateToMainActivity();
-        } else {
-            // Mostrar pantalla de inicio de sesión
-            setContentView(R.layout.activity_login);
-            usernameField = findViewById(R.id.usernameField);
-        }
+        usernameField = findViewById(R.id.usernameField);
     }
 
     public void onLoginClick(View view) {
@@ -36,16 +26,8 @@ public class LoginActivity extends AppCompatActivity {
         if (username.isEmpty()) {
             Toast.makeText(this, "Por favor, ingresa un nombre de usuario", Toast.LENGTH_SHORT).show();
         } else {
-            saveSession(username);
             navigateToMainActivity();
         }
-    }
-
-    private void saveSession(String username) {
-        SharedPreferences preferences = getSharedPreferences("auth_prefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("username", username);
-        editor.apply();
     }
 
     private void navigateToMainActivity() {
